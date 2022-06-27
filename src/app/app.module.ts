0c3e6646a5f46c993140517850e3cdf0e1f0664f
pandikapinata <pandikapinata27@gmail.com>
2022-06-27 13:17:48 +0800

    Test Pipe and implement beforeEach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StrengthPipe } from './pipes/Strength/strength.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StrengthPipe
   ],
   imports: [
     BrowserModule
--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -2,21 +2,24 @@ import { LoggerService } from '../Logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+  beforeEach(() => {
+    console.log('calling before each');
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  });
+
   it('should add two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
-
+    console.log('calling add');
     let result = calculator.add(2, 2);
-
     expect(result).toBe(4);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
+
   it('should substract two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
-
+    console.log('calling substract');
     let result = calculator.substract(2, 2);
-
     expect(result).toBe(0);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
