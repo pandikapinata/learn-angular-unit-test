1c35f93f498820ff820d769b33de67d10641faaf
pandikapinata <pandikapinata27@gmail.com>
2022-06-27 12:37:06 +0800

    7. Testing service which has another service (D.Injection

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -1,15 +1,18 @@
+import { LoggerService } from '../Logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    const calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    const calculator = new CalculatorService(loggerService);
 
     let result = calculator.add(2, 2);
 
     expect(result).toBe(4);
   });
   it('should substract two numbers', () => {
-    const calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    const calculator = new CalculatorService(loggerService);
 
     let result = calculator.substract(2, 2);
 
